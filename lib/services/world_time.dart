456b901d13a3bce018e0c77f611f14af83f7041c
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-08-19 02:33:08 +0530

    Offset adjustment to report correct time

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -17,19 +17,19 @@ class WorldTime{
       Response response = await get(
           Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
       Map data = jsonDecode(response.body);
-      //print(data);
+      print(data);
 
       //get the properties
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
+      String offset = data['utc_offset'].substring(0, 3);
 
-      // print(datetime);
-      // print(offset);
+      print(datetime);
+      print(offset);
 
       //create a datetime object
 
       DateTime now = DateTime.parse(datetime);
-      now = now.add(Duration(hours: int.parse(offset)));
+      now = (offset[0]=='+'? now.add(Duration(hours: int.parse(offset.substring(1,3)))) : now.subtract(Duration(hours: int.parse(offset.substring(1,3)))));
       isday=now.hour>6 && now.hour<18?true:false;
       time = DateFormat.jm().format(now);
 
